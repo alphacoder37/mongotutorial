d9a2a85bd1aa458c5b91d9f661edc8a8fd9a6978
Leon Cummins <leon.cummins@gmail.com>
2021-09-16 14:09:34 +1000

    update

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(process.env)
 
 let Person;
 
